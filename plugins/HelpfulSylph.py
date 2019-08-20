e1c909442fa058897c798099eafab34096855484
Speedy321 <Christo-Chibougamo@hotmail.com>
2019-08-20 15:19:26 -0400

    Initial bot
    First code commit

---
+++ plugins/HelpfulSylph.py
@@ -0,0 +1,9 @@
+#Start with 
+# >py -m disco.cli --token [Your discord bot token] --config config.json
+
+from disco.bot import Plugin
+
+class HelpfulSylph(Plugin):
+    @Plugin.command('ping')
+    def command_ping(self, event):
+        event.msg.reply('Pong!')
