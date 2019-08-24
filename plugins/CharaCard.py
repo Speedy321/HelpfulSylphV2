ae7e826ceee57f377233a0a7045a9ed60f0ef89a
Speedy321 <Christo-Chibougamo@hotmail.com>
2019-08-24 16:46:19 -0400

    Adding error checks for CharaCard
    A lot of (often behaviour braking) missing error checks

--- plugins/CharaCard.py
+++ plugins/CharaCard.py
@@ -26,7 +26,7 @@ class CharaCard:
     jsonData = ""
 
     def __init__(self, charID):
-        self.charID = charID
+        self.charID = int(charID)
         #url = self.fecthApiUrl+charID
         #self.jsonData = requests.post(url).json()
         #print(self.jsonData)
@@ -35,8 +35,11 @@ class CharaCard:
 
         if self.DEBUG:
             print("[debug]CharaCard.getCardMsg()")
-        data = dbSingle.getCardByUserID(self.charID)
-        if data :
+
+        if self.charID > 0:
+            data = dbSingle.getCardByUserID(self.charID)
+            if data :
+            
                 if self.DEBUG:
                     print("[debug]CharaCard.getCardMsg() data:")
                     print(data)
@@ -46,13 +49,13 @@ class CharaCard:
                     self.fetchCardFromGenerator()
                 else:
                     self.cardID = data[1]
-        else:
+            else:
 
                 if self.DEBUG:
                     print("[debug]CharaCard.getCardMsg() Empty data")
-            self.fetchCardFromGenerator()
 
-        if self.cardID > 0:
+                self.fetchCardFromGenerator()
+
             self.embedMsg.image = MessageEmbedImage(url = self.cardGeneratorApiCardUrl + str(self.cardID))
 
         return self.embedMsg
@@ -61,11 +64,13 @@ class CharaCard:
         
         if self.DEBUG:
             print("[debug]CharaCard.fetchCardFromGenerator() ID = "+str(self.charID))
-        url = self.cardGeneratorApiUrl + str(self.charID) + self.cardGeneratorApiEndUrl
-        response = requests.get(url)
-        match = re.search("(?:https:\/\/www\.ffxivprofilegenerator\.com\/get\/)(\d{1,10})", response.text)
-        self.cardID = int(match.group(1))
-        dbSingle.addOrUpdateCard(self.charID, self.cardID)
+        
+        if self.charID > 0:
+            url = self.cardGeneratorApiUrl + str(self.charID) + self.cardGeneratorApiEndUrl
+            response = requests.get(url)
+            match = re.search("(?:https:\/\/www\.ffxivprofilegenerator\.com\/get\/)(\d{1,10})", response.text)
+            self.cardID = int(match.group(1))
+            dbSingle.addOrUpdateCard(self.charID, self.cardID)
 
         
 
